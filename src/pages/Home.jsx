d3da7b221abb7b1ad79537075474908b320d063d
A V Guruprasad <guruprasad6282@gmail.com>
2026-05-23 16:00:42 +0530

    responsive update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,7 +14,6 @@ const Home = () => {
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
       transition={{ duration: 0.5 }}
-      className="page-wrapper"
     >
       <HeroSection />
       <AboutSection />
